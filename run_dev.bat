192167d20bae6fb7aa00941158304b7619d28199
attackonFourier <543098332@qq.com>
2016-09-07 18:39:23 +0800

    Update run_dev.bat

--- run_dev.bat
+++ run_dev.bat
@@ -1,3 +1,8 @@
+/*
+ *start with weex:http://weex.help/topic/577a5b44eb60516a48db5499
+ *window can't operate two dev,
+ *so this bat doc is to fixed this problem.
+ **/
 @echo off 
 h:
 cd traing\weex\demo03
